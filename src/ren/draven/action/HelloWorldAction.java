5f4e15953b61f6d4934d93f1d487629dfa5dde7a
dravenxiaokai <jspankai@vip.qq.com>
2018-03-28 15:37:42 +0800

    struts2项目初步搭建

---
+++ src/ren/draven/action/HelloWorldAction.java
@@ -0,0 +1,23 @@
+package ren.draven.action;
+
+import java.net.URLEncoder;
+
+public class HelloWorldAction {
+	public String getUserName() {
+		return userName;
+	}
+
+	private String msg;
+	private String userName;
+
+	public String getMessage() {
+		return msg;
+	}
+
+	public String execute() throws Exception {
+		this.msg = "我的第一个struts应用";
+		this.userName = URLEncoder.encode("传智播客","UTF-8");
+		
+		return "success";
+	}
+}
